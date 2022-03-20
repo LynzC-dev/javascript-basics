5f653b26d898c2be0380b80dad3a416411b46c1a
Lyndsey Coombs <lyndscoombs@aol.com>
2022-03-20 14:58:27 +0000

    Booleans 75% complete

--- src/booleans.js
+++ src/booleans.js
@@ -1,49 +1,74 @@
 function negate(a) {
   // your code here
+  return (!a);
 };
 
 function both(a, b) {
   // your code here
+  return a && b;
+
 };
 
 function either(a, b) {
   // your code here
+  return a || b;
 };
 
 function none(a, b) {
   // your code here
+  return !(a || b);
 };
 
 function one(a, b) {
-  // your code here
+  // your code here\
+  if ((a === true && b === false) || (a === false && b === true)) {
+    return true;
+  }
+    else return false;
 };
 
 function truthiness(a) {
   // your code here
+  return Boolean(a);
 };
 
 function isEqual(a, b) {
+  if (a ===b)  {
+    return true;
+  }
+  else return false;
   // your code here
 };
 
 function isGreaterThan(a, b) {
   // your code here
+  return a > b;
 };
 
 function isLessThanOrEqualTo(a, b) {
   // your code here
+  return a <= b;
 };
 
 function isOdd(a) {
   // your code here
+  if (a%2 > 0) {
+    return true;
+  }
+  else return false;
 };
 
 function isEven(a) {
   // your code here
+  if (a%2 === 0) {
+    return true;
+  }
+  else return false;
 };
 
 function isSquare(a) {
   // your code here
+ 
 };
 
 function startsWith(char, string) {
